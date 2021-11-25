9c8ca8f66c32269eaa4ea0b5b5faa9c68296264e
Hing <32828455+gddcx@users.noreply.github.com>
2021-11-25 17:12:42 +0800

    Update eval.py

--- yolov1/resnet/eval.py
+++ yolov1/resnet/eval.py
@@ -165,11 +165,8 @@ def load_data(DATA_PATH):
 
 
 if __name__ == "__main__":
-    # DATA_PATH="../../dataset/VOC2007/test/"
     DATA_PATH="D:\\dataset\\VOC2007\\test"
-    # DATA_PATH="D:\\dataset\\VOC2012\\test"
-    # MODEL_PATH = "models/YOLO_ResNet50Det/294/90_25116.pth"
-    MODEL_PATH = "models/YOLO_ResNet50Det/296/113_31464.pth"
+    MODEL_PATH = "resnetdet.pth"
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
